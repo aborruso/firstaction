371f674b36df8d34a0fdf86fcc33a2305a261b70
Andrea Borruso <aborruso@gmail.com>
2020-06-20 11:39:22 +0200

    crea gitlog dei file in TSV

---
+++ gitlog.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+rm "$folder"/gitlog.tsv
+
+for i in ./*.csv; do
+  #crea una variabile da usare per estrarre nome e estensione
+  filename=$(basename "$i")
+  #estrai estensione
+  extension="${filename##*.}"
+  #estrai nome file
+  filename="${filename%.*}"
+  echo "$filename.$extension"
+  git log --date=iso --no-merges --pretty=format:"%h%x09%an%x09%ad%x09%s" -- "$i" | ./mlr --tsv -N put '$filename="'"$filename"'"' >>"$folder"/gitlog.tsv
+done
+
+./mlr -I --tsv -N sort -f 5,3 "$folder"/gitlog.tsv
+
