5127131b92438e0bd2d0fc87f5cf595c839b88c0
Andrea Borruso <aborruso@gmail.com>
2020-06-20 16:46:29 +0200

    update

--- gitlog.sh
+++ gitlog.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+set -x
+
 folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-rm "$folder"/gitlog.tsv
+mkdir -p "$folder"/rawdata/gitlog
 
 for i in ./*.csv; do
   #crea una variabile da usare per estrarre nome e estensione
@@ -12,8 +14,28 @@ for i in ./*.csv; do
   #estrai nome file
   filename="${filename%.*}"
   echo "$filename.$extension"
-  git log origin/master --date=iso --no-merges --pretty=format:"%h%x09%an%x09%ad%x09%s" -- "$i" | ./mlr --tsv -N put '$filename="'"$filename"'"' >>"$folder"/gitlog.tsv
+  echo "commit id,author,date,comment,changed files,linesAdded,linesDeleted" >"$folder"/rawdata/gitlog/"$filename".csv
+  git log --date=iso --all --no-merges --pretty="%x40%h%x2C%an%x2C%ad%x2C%x22%s%x22%x2C" --shortstat -- "$i" | tr "\n" " " | tr "@" "\n" >>"$folder"/rawdata/gitlog/"$filename".csv
+  sed -i 's/ files changed//g' "$folder"/rawdata/gitlog/"$filename".csv
+  sed -i 's/ file changed//g' "$folder"/rawdata/gitlog/"$filename".csv
+  sed -i 's/ insertions(+)//g' "$folder"/rawdata/gitlog/"$filename".csv
+  sed -i 's/ insertion(+)//g' "$folder"/rawdata/gitlog/"$filename".csv
+  sed -i 's/ deletions(-)//g' "$folder"/rawdata/gitlog/"$filename".csv
+  sed -i 's/ deletion(-)//g' "$folder"/rawdata/gitlog/"$filename".csv
+  sed -i '/^$/d' "$folder"/rawdata/gitlog/"$filename".csv
+  mlr -I --csv --allow-ragged-csv-input clean-whitespace then put '$fileName="'"$filename"'"' "$folder"/rawdata/gitlog/"$filename".csv
 done
 
-./mlr -I --tsv -N sort -f 3,5 "$folder"/gitlog.tsv
+mlr --c2t sort -f date "$folder"/rawdata/gitlog/*.csv >"$folder"/gitlog.tsv
 
+echo "commitId,author,date,comment,changedFiles,linesAdded,linesDeleted" >./tmp_res
+git log --date=iso --all --no-merges --pretty="%x40%h%x2C%an%x2C%ad%x2C%x22%s%x22%x2C" --shortstat -- '*.csv' | tr "\n" " " | tr "@" "\n" >>./tmp_res
+sed -i 's/ files changed//g' ./tmp_res
+sed -i 's/ file changed//g' ./tmp_res
+sed -i 's/ insertions(+)//g' ./tmp_res
+sed -i 's/ insertion(+)//g' ./tmp_res
+sed -i 's/ deletions(-)//g' ./tmp_res
+sed -i 's/ deletion(-)//g' ./tmp_res
+sed -i '/^$/d' ./tmp_res
+
+mlr -I --csv --allow-ragged-csv-input clean-whitespace then sort -f date "$folder"/tmp_res
