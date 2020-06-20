33067836058d7b8dbd1e7474d8bbf4c054a41e7c
Andrea Borruso <aborruso@gmail.com>
2020-06-20 12:01:32 +0200

    update

--- gitlog.sh
+++ gitlog.sh
@@ -3,6 +3,7 @@
 folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 rm "$folder"/gitlog.tsv
+rm "$folder"/gitlogt.tsv
 
 for i in ./*.csv; do
   #crea una variabile da usare per estrarre nome e estensione
@@ -13,6 +14,7 @@ for i in ./*.csv; do
   filename="${filename%.*}"
   echo "$filename.$extension"
   git log --date=iso --no-merges --pretty=format:"%h%x09%an%x09%ad%x09%s" -- "$i" | ./mlr --tsv -N put '$filename="'"$filename"'"' >>"$folder"/gitlog.tsv
+  git log --date=iso --no-merges --pretty=format:"%h%x09%an%x09%ad%x09%s" -- "$i" >>"$folder"/gitlogt.tsv
 done
 
 ./mlr -I --tsv -N sort -f 3,5 "$folder"/gitlog.tsv
