62231880576bfa74a120802f66c1eede272abe32
Andrea Borruso <aborruso@gmail.com>
2020-06-20 16:50:55 +0200

    update

--- gitlog.sh
+++ gitlog.sh
@@ -23,10 +23,10 @@ for i in ./*.csv; do
   sed -i 's/ deletions(-)//g' "$folder"/rawdata/gitlog/"$filename".csv
   sed -i 's/ deletion(-)//g' "$folder"/rawdata/gitlog/"$filename".csv
   sed -i '/^$/d' "$folder"/rawdata/gitlog/"$filename".csv
-  mlr -I --csv --allow-ragged-csv-input clean-whitespace then put '$fileName="'"$filename"'"' "$folder"/rawdata/gitlog/"$filename".csv
+  ./mlr -I --csv --allow-ragged-csv-input clean-whitespace then put '$fileName="'"$filename"'"' "$folder"/rawdata/gitlog/"$filename".csv
 done
 
-mlr --c2t sort -f date "$folder"/rawdata/gitlog/*.csv >"$folder"/gitlog.tsv
+./mlr --c2t sort -f date "$folder"/rawdata/gitlog/*.csv >"$folder"/gitlog.tsv
 
 echo "commitId,author,date,comment,changedFiles,linesAdded,linesDeleted" >./tmp_res
 git log --date=iso --all --no-merges --pretty="%x40%h%x2C%an%x2C%ad%x2C%x22%s%x22%x2C" --shortstat -- '*.csv' | tr "\n" " " | tr "@" "\n" >>./tmp_res
@@ -38,4 +38,4 @@ sed -i 's/ deletions(-)//g' ./tmp_res
 sed -i 's/ deletion(-)//g' ./tmp_res
 sed -i '/^$/d' ./tmp_res
 
-mlr -I --csv --allow-ragged-csv-input clean-whitespace then sort -f date "$folder"/tmp_res
+./mlr -I --csv --allow-ragged-csv-input clean-whitespace then sort -f date "$folder"/tmp_res
