64d21e0e24515aef3751248e18213d2453884551
Andrea Borruso <aborruso@gmail.com>
2020-06-21 22:20:52 +0200

    creazione CSV con insertion e deletion da log git

--- gitlog.sh
+++ gitlog.sh
@@ -32,13 +32,13 @@ mlr --c2t sort -r date then put '$URL="https://github.com/aborruso/firstaction/c
 
 mkdir -p "$folder"/rawdata
 
-git log --since="2020-06-21T14:04:51 +0000" --date=iso --all --no-merges  \
+git log --since="2020-06-21T13:03:51 +0000" --date=iso --all --no-merges \
   --pretty=format:'{%n  "commit": "%H",%n  "author": "%aN <%aE>",%n  "date": "%ad",%n  "message": "%f"%n},' \
   -- '*.csv' |
   perl -pe 'BEGIN{print "["}; END{print "]\n"}' |
   perl -pe 's/},]/}]/' | jq . >"$folder"/rawdata/gitlog.json
 
-git log --since="2020-06-21T14:04:51 +0000" --date=iso --all --no-merges  \
+git log --since="2020-06-21T13:03:51 +0000" --date=iso --all --no-merges \
   --numstat \
   --format='%H' \
   -- '*.csv' |
@@ -55,7 +55,9 @@ git log --since="2020-06-21T14:04:51 +0000" --date=iso --all --no-merges  \
   perl -wpe 's#(]|}),\s*(]|})#$1$2#g' |
   perl -wpe 's#,\s*?}$#}#' | jq . >"$folder"/rawdata/gitlogstat.json
 
-jq --slurp '.[1] as $logstat | .[0] | map(.paths = $logstat[.commit])' "$folder"/rawdata/gitlog.json "$folder"/rawdata/gitlogstat.json | jq -c '.[]' >"$folder"/log.json
+jq --slurp '.[1] as $logstat | .[0] | map(.paths = $logstat[.commit])' "$folder"/rawdata/gitlog.json "$folder"/rawdata/gitlogstat.json >"$folder"/tmplog.json
+jq <"$folder"/tmplog.json -c '.[]' >"$folder"/log.json
 
+mlr <"$folder"/log.json --j2c unsparsify >"$folder"/tmplog.csv
 
-
+mlr <"$folder"/tmplog.csv --csv cat -n then reshape -r ":" -o item,value then filter -x '$value==""' then put '$item=sub($item,"paths:","")' then nest --explode --values --across-fields --nested-fs ":"  -f item then reshape -s item_2,value then cut -x -f n,item_1 then sort -r date then filter '$author=~"actions"' >"$folder"/tmp2.csv
