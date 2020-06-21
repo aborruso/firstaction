978412b16dd90cdcc1b93c3fb9b63da5417f7580
Andrea Borruso <aborruso@gmail.com>
2020-06-21 17:14:23 +0200

    update

--- log.sh
+++ log.sh
@@ -10,4 +10,4 @@ git log \
     --pretty=format:'{%n  "commit": "%H",%n  "author": "%aN <%aE>",%n  "date": "%ad",%n  "message": "%f"%n},' \
     $@ | \
     perl -pe 'BEGIN{print "["}; END{print "]\n"}' | \
-    perl -pe 's/},]/}]/' | jq -c '.[]' >"$folder"/rawdata/log.jsonl
+    perl -pe 's/},]/}]/' | jq -c '.[]' >"$folder"/rawdata/gitlog.jsonl
