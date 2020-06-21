ab5df4a3c242b9be406f141a9974c02c9d28b661
Andrea Borruso <aborruso@gmail.com>
2020-06-21 17:33:22 +0200

    update

--- gitlog.sh
+++ gitlog.sh
@@ -28,4 +28,31 @@ done
 
 mlr --c2t sort -r date then put '$URL="https://github.com/aborruso/firstaction/commit/".$commitId' "$folder"/rawdata/gitlog/*.csv >"$folder"/gitlog.tsv
 
-# git log --since="2020-06-18T21:35:51 +0000"   --date=iso --all --no-merges --pretty="%x40%h%x2C%an%x2C%ad%x2C%x22%s%x22%x2C" --shortstat  | tr "\n" " " | tr "@" "\n"
+# altrà modalità di creazione log, in formato json
+
+mkdir -p "$folder"/rawdata
+
+git log --since="2020-06-18T21:35:51 +0000" --date=iso --all --no-merges  \
+  --pretty=format:'{%n  "commit": "%H",%n  "author": "%aN <%aE>",%n  "date": "%ad",%n  "message": "%f"%n},' \
+  -- '*.csv' |
+  perl -pe 'BEGIN{print "["}; END{print "]\n"}' |
+  perl -pe 's/},]/}]/' | jq . >"$folder"/rawdata/gitlog.json
+
+git log --since="2020-06-18T21:35:51 +0000" --date=iso --all --no-merges  \
+  --numstat \
+  --format='%H' \
+  -- '*.csv' |
+  perl -lawne '
+        if (defined $F[1]) {
+            print qq#{"insertions": "$F[0]", "deletions": "$F[1]", "path": "$F[2]"},#
+        } elsif (defined $F[0]) {
+            print qq#],\n"$F[0]": [#
+        };
+        END{print qq#],#}' |
+  tail -n +2 |
+  perl -wpe 'BEGIN{print "{"}; END{print "}"}' |
+  tr '\n' ' ' |
+  perl -wpe 's#(]|}),\s*(]|})#$1$2#g' |
+  perl -wpe 's#,\s*?}$#}#' | jq . >"$folder"/rawdata/gitlogstat.json
+
+jq --slurp '.[1] as $logstat | .[0] | map(.paths = $logstat[.commit])' "$folder"/rawdata/gitlog.json "$folder"/rawdata/gitlogstat.json | jq -c '.[]' >"$folder"/log.json
